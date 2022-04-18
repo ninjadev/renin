a7442907d4041d6050bda1522bc8269205f877ce
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2022-04-19 01:37:52 +0200

    Further iterate on example project

--- example-project/src/postfx.glsl
+++ example-project/src/postfx.glsl
@@ -8,6 +8,18 @@ void main() {
     /* contrast adjustment */
     color = pow(0.25 + color, vec3(1.5));
 
+    /* vignette */
+    color *= 1. - 3. * pow(length(vUv - 0.5), 4.);
+
+    /* scanlines */
+    float intensity = (color.r + color.g + color.b) / 3.;
+    vec3 scanlineColor = color * (1. - 0.02 * smoothstep(0.2, 1., sin(-time * .5 + vUv.y * 212.)));
+    color = mix(color, scanlineColor, 1. - pow(intensity ,4.));
+
+    /* color tint */
+    color += vec3(0.05, 0.05, 0.1);
+
+    /* fade in from black */
     color = mix(vec3(0.), color, smoothstep(125., 252., time));
 
     gl_FragColor = vec4(color, 1.);
