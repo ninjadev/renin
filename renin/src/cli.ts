3834064411fb63fa9974583a2138baf4712b8673
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2022-04-19 23:09:24 +0200

    Implement npx renin init <name>

--- renin/src/cli.ts
+++ renin/src/cli.ts
@@ -1,4 +1,6 @@
 import fs from 'fs/promises';
+import path from 'path';
+import url from 'url';
 
 function printHelp() {
   console.log('Usage: npx renin init <name>');
@@ -6,15 +8,26 @@ function printHelp() {
 
 async function init(name: string) {
   console.log(`Creating new renin project in ${name}...`);
+  const base = path.dirname(url.fileURLToPath(import.meta.url));
+  try {
+    await fs.access(name);
+    console.log(`${name} already exists, aborting.`);
+    process.exit(1);
+  } catch {}
   await fs.mkdir(name, { recursive: true });
+  await fs.cp(path.join(base, 'example-project'), name, { recursive: true });
   console.log('Project set up!');
 }
 
 export async function main() {
-  console.log(process.argv);
   if (process.argv.length == 4 && process.argv[2] === 'init') {
     const name = process.argv[3];
-    await init(name);
+    try {
+      await init(name);
+    } catch (e) {
+      console.log('Sadly, something went wrong. Sorry!');
+      throw e;
+    }
     process.exit(0);
   } else {
     printHelp();
